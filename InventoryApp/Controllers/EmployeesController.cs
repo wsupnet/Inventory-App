ba052531b46f9832df0c0172f89619cb90e89def
Wes <Wes@DESKTOP-16T4F05>
2017-11-16 21:11:45 -0800

    Added a manage records column on employees index view

--- InventoryApp/Controllers/EmployeesController.cs
+++ InventoryApp/Controllers/EmployeesController.cs
@@ -19,6 +19,8 @@ namespace InventoryApp.Controllers
         // GET: Employees
         public ActionResult Index()
         {
+            ViewBag.datasource = db.Employees.ToList();
+
             return View(db.Employees.ToList());
         }
 
